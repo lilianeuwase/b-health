55ef46e68afffc2f005976cd8fb3f22d6531bb4c
lilianeuwase <38309956+lilianeuwase@users.noreply.github.com>
2022-10-04 20:09:11 +0200

    4-Oct

--- src/components/pages/Services/Primary.js
+++ src/components/pages/Services/Primary.js
@@ -1,74 +1,32 @@
 import React from 'react';
-import GoogleFontLoader from 'react-google-font-loader';
-import NoSsr from '@material-ui/core/NoSsr';
-import { makeStyles } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
-import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
-import {
-  Info,
-  InfoCaption,
-  InfoSubtitle,
-  InfoTitle,
-} from '@mui-treasury/components/info';
-import { useGalaxyInfoStyles } from '@mui-treasury/styles/info/galaxy';
-import { useCoverCardMediaStyles } from '@mui-treasury/styles/cardMedia/cover';
+import CardItem2 from '../../CardItem2';
+import '../Services.css';
 
-const useStyles = makeStyles(() => ({
-  card: {
-    borderRadius: '1rem',
-    boxShadow: 'none',
-    position: 'relative',
-    minWidth: 200,
-    minHeight: 360,
-    '&:after': {
-      content: '""',
-      display: 'block',
-      position: 'absolute',
-      width: '100%',
-      height: '64%',
-      bottom: 0,
-      zIndex: 1,
-      background: 'linear-gradient(to top, #000, rgba(0,0,0,0))',
-    },
-  },
-  content: {
-    position: 'absolute',
-    zIndex: 2,
-    bottom: 0,
-    width: '100%',
-  },
-}));
 
-export const Primary = React.memo(function GalaxyCard() {
-  const mediaStyles = useCoverCardMediaStyles({ bgPosition: 'top' });
-  const styles = useStyles();
+function Primary() {
   return (
-    <>
-      <NoSsr>
-        <GoogleFontLoader
-          fonts={[
-            { font: 'Spartan', weights: [300] },
-            { font: 'Montserrat', weights: [200, 400, 700] },
-          ]}
-        />
-      </NoSsr>
-      <Card className={styles.card}>
-        <CardMedia
-          classes={mediaStyles}
-          image={
-            'https://image-us.samsung.com/SamsungUS/home/audio/galaxy-buds/MB-04-JustWhatYouWantV4.jpg?$cm-g-fb-full-bleed-img-mobile-jpg$'
-          }
-        />
-        <Box py={3} px={2} className={styles.content}>
-          <Info useStyles={useGalaxyInfoStyles}>
-            <InfoSubtitle>Galaxy</InfoSubtitle>
-            <InfoTitle>Buds 2019</InfoTitle>
-            <InfoCaption>Perfect for everyone</InfoCaption>
-          </Info>
-        </Box>
-      </Card>
-    </>
+    <div className='cards'>
+      <h1>Our Services</h1>
+      <div className='cards_container'>
+        <div className='cards_wrapper'>
+          <ul className='cards_items'>
+            <CardItem2
+              subtitle='Health Services that Cover a Range of Prevention, Wellness, and Treatment for Common Illnesses'
+              title='Primary Care'
+              caption='primary'
+            />
+          </ul>
+          <ul className='cards_items'>
+            <CardItem2
+              subtitle='Health Services that Cover a Range of Prevention, Wellness, and Treatment for Common Illnesses'
+              title='Primary Care'
+              caption='primary'
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
   );
-});
+}
+
 export default Primary;
